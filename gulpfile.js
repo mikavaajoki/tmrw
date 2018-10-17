48bb9b3ee1edbac18e023b7573b04b98fe621126
Daniel Adeyemi <danieladeyemi@Daniels-MacBook-Pro-2.local>
2018-10-17 12:20:14 +0200

    article-header

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,21 @@ var autoprefixer = require('autoprefixer');
 var postcssPresetEnv = require('postcss-preset-env');
 var postcssNesting = require('postcss-nesting');
 var postcssMediaMixmax = require('postcss-media-minmax');
+var concatCss = require('gulp-concat-css');
+var cssnano = require('gulp-cssnano');
+
 
 gulp.task('css', function () {
-  console.log('happens css');
     var plugins = [postcssPresetEnv, postcssNesting, postcssMediaMixmax];
     return gulp.src('wordpress/wp-content/themes/tmrw-wp-theme/css/*.css')
         .pipe(postcss(plugins))
+        .pipe(concatCss("bundle.css"))
+        .pipe(cssnano())
         .pipe(gulp.dest('wordpress/wp-content/themes/tmrw-wp-theme/dist/css'));
 });
 
 
 gulp.task('watch', function() {
-    console.log('happens');
-
  gulp.watch('wordpress/wp-content/themes/tmrw-wp-theme/css/*.css', ['css']);
 });
 
