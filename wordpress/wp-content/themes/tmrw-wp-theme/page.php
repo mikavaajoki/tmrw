5d1a06424468eeb483a30659fc24104c8bc2d4a1
Daniel Adeyemi <danieladeyemi@Daniels-MBP-2.lan>
2018-10-18 21:56:44 +0200

    grid item

--- wordpress/wp-content/themes/tmrw-wp-theme/page.php
+++ wordpress/wp-content/themes/tmrw-wp-theme/page.php
@@ -5,6 +5,6 @@
   $context['post'] = $post;
   $args = array('post_type' => 'post', 'posts_per_page' => 100);   
 	$context['posts'] = Timber::get_posts($args);
-  Timber::render('page.twig', $context );
-
+	Timber::render( array('page-' . $post->post_name . '.twig','page.twig'), $context );
 ?>
+
