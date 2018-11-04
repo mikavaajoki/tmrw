57c179320205594938439610449cad9d3e37e479
Daniel Adeyemi <danieladeyemi@Daniels-MacBook-Pro-2.local>
2018-11-04 18:12:13 +0000

    header and footer done

--- wordpress/wp-content/themes/tmrw-wp-theme-staging/category.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$current_category = single_cat_title( '', false);
-	$context = Timber::get_context();
-	$args = array('post_type' => 'post', 'posts_per_page' => 100,'category_name' => ''.$current_category.'');   
-	$context['posts'] = Timber::get_posts($args);
-	$context['categories'] = Timber::get_terms('category', array('orderby' => 'name','order' => 'ASC'));
-	Timber::render('category.twig', $context );
-?>
--- wordpress/wp-content/themes/tmrw-wp-theme-staging/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$context = Timber::get_context();
-
-$context['pagination'] = Timber::get_pagination();
-
-Timber::render('index.twig', $context);
-
-?>
