fd1223a1cecc93b4fc17715cc789dc76d5af3223
Daniel Adeyemi <danieladeyemi@Daniels-MacBook-Pro-2.local>
2018-11-10 00:43:03 +0000

    cart conversion

---
+++ wordpress/wp-content/themes/tmrw-wp-theme/js/woo-quantity.js
@@ -0,0 +1,9 @@
+/**
+*	Woo quantity Buttons
+*	Version: 1.0.0
+*	Description: Adds quantity buttons for WooCommerce
+*	Author: Greatives
+*/
+
+!function(t){t(document).ready(function(){function e(){t(".woocommerce .quantity input[type=number]").each(function(){var e=t(this),n=parseFloat(e.attr("max")),a=parseFloat(e.attr("min")),r=parseInt(e.attr("step"),10),u=t(t("<div />").append(e.clone(!0)).html().replace("number","text")).insertAfter(e);e.remove(),setTimeout(function(){if(0==u.next(".plus").length){var e=t('<input type="button" value="-" class="quantity-button minus">').insertBefore(u),i=t('<input type="button" value="+" class="quantity-button plus">').insertAfter(u);e.on("click",function(){var t=parseInt(u.val(),10)-r;t=0>t?0:t,t=a>t?a:t,u.val(t).trigger("change")}),i.on("click",function(){var t=parseInt(u.val(),10)+r;t=t>n?n:t,u.val(t).trigger("change")})}},10)})}e(),t(document).on("updated_cart_totals",e)})}(jQuery);
+
