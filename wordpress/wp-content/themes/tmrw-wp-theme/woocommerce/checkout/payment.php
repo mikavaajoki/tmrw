15edf153c3c77ee625ac640d6d70d83ea6ea988f
Daniel Adeyemi <danieladeyemi@Daniels-MacBook-Pro-2.local>
2018-11-11 20:24:59 +0000

    stripe now works

--- wordpress/wp-content/themes/tmrw-wp-theme/woocommerce/checkout/payment.php
+++ wordpress/wp-content/themes/tmrw-wp-theme/woocommerce/checkout/payment.php
@@ -21,9 +21,15 @@ if ( ! is_ajax() ) {
 	do_action( 'woocommerce_review_order_before_payment' );
 }
 ?>
-<div id="payment" class="woocommerce-checkout-payment">
+<div class="woocommerce-checkout-payment payment-method">
+	<div class="section-heading">
+		<h4>Payment Method</h4>
+	</div>
+
+
+
+
 	<?php if ( WC()->cart->needs_payment() ) : ?>
-		<ul class="wc_payment_methods payment_methods methods">
 			<?php
 			if ( ! empty( $available_gateways ) ) {
 				foreach ( $available_gateways as $gateway ) {
@@ -33,8 +39,12 @@ if ( ! is_ajax() ) {
 				echo '<li class="woocommerce-notice woocommerce-notice--info woocommerce-info">' . apply_filters( 'woocommerce_no_available_payment_methods_message', WC()->customer->get_billing_country() ? esc_html__( 'Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) : esc_html__( 'Please fill in your details above to see available payment methods.', 'woocommerce' ) ) . '</li>'; // @codingStandardsIgnoreLine
 			}
 			?>
-		</ul>
 	<?php endif; ?>
+	
+
+
+
+
 	<div class="form-row place-order">
 		<noscript>
 			<?php esc_html_e( 'Since your browser does not support JavaScript, or it is disabled, please ensure you click the <em>Update Totals</em> button before placing your order. You may be charged more than the amount stated above if you fail to do so.', 'woocommerce' ); ?>
@@ -51,6 +61,9 @@ if ( ! is_ajax() ) {
 
 		<?php wp_nonce_field( 'woocommerce-process_checkout', 'woocommerce-process-checkout-nonce' ); ?>
 	</div>
+
+
+
 </div>
 <?php
 if ( ! is_ajax() ) {
