b20f7469a762f70a03efa313c7feee6a75142be8
Daniel Adeyemi <danieladeyemi@Daniels-MacBook-Pro-2.local>
2018-11-13 01:11:58 +0000

    trying to figure out link

--- wordpress/wp-content/themes/tmrw-wp-theme/front-page.php
+++ wordpress/wp-content/themes/tmrw-wp-theme/front-page.php
@@ -3,6 +3,8 @@
   $post = new TimberPost();
   $args = array('post_type' => array('post','legacy'), 'posts_per_page' => 100);   	
   $issueArgs = array('post_type' => 'issue', 'posts_per_page' => 100);   
+  $productArgs = array('post_type' => 'product', 'posts_per_page' => 100);   
+  $context['issueProds'] = Timber::get_posts($productArgs);
   $context['issues'] = Timber::get_posts($issueArgs);
 	$context['posts'] = Timber::get_posts($args);
   $context['post'] = $post;
